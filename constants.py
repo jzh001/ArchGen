7bf2348dc41b57e9ec5834cbaa8972110ecb404c
jzh001 <jiangzhsg@gmail.com>
2025-09-10 20:15:21 +0800

    update model sequence

--- constants.py
+++ constants.py
@@ -2,10 +2,10 @@ RUBRICS_PROMPT_PATH = 'prompts/rubrics.txt'
 GENERATOR_PROMPT_PATH = 'prompts/generator.txt'
 CRITIC_PROMPT_PATH = 'prompts/critic.txt'
 LLM_OPTIONS = [
-    "ollama:gpt-oss:20b",
+    # "ollama:gpt-oss:20b",
     # "ollama:qwen3:8b",
-    "anthropic:claude-4-sonnet",
     "google-genai:gemini-2.5-flash",
     "google-genai:gemini-2.5-flash-lite",
+    "anthropic:claude-4-sonnet",
 ]
 MAX_ITER = 5
